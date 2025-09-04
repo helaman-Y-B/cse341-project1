b3bc0dfc5c13d9cc4e6fd79f086392d6563b94c9
Helamã Y. Barbour <helamashi@gmail.com>
2025-09-04 11:41:46 -0300

    installed dotenv and stated creating the json file for the data and store it into a collection in mongo DB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
